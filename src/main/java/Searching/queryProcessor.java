5b3eff20d2471706cb568e9356a6aaf16ad2386a
unknown <amrsaad867@gmail.com>
2024-04-26 02:01:16 +0100

    final

--- src/main/java/Searching/queryProcessor.java
+++ src/main/java/Searching/queryProcessor.java
@@ -1,6 +1,8 @@
 package Searching;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -18,43 +20,43 @@ class Index {
         ArrayList<Document> ret = new ArrayList<>();
        if(isQoutes)
         {
-            Set<Document> uniqueDocuments = new HashSet<>();
             for (String word : stems) {
                 // Retrieve documents containing statements for each word
                 ArrayList<Document> result = DB.getDocumentsContainingWord(word, "Indexer");
-    
+                if(result.isEmpty())
+                continue;
+                Double df=1.0/(double)result.get(0).getInteger("documentCount");
                 // Search within the "statements" field of each document for the actual sentence
                 // that was sent in the query
                 for (Document doc : result) {
                     List<Document> docs = (ArrayList<Document>) doc.get("documents");
-                    Double df=1.0/(double)result.get(0).getInteger("documentCount");
                     for (Document doc1 : docs) {
                         List<Document> statements = (ArrayList<Document>) doc1.get("statements");
                         if (statements != null) {
-                            boolean containsAllWords = true; // Initialize flag for each document
+                            boolean containsAllWords = false; // Initialize flag for each document
                             for (Document statement : statements) {
                                 String state = statement.getString("state");
                                 // Check if all words in the query are present in the statement
-                                if (!state.contains(ifQuotes)) {
-                                    containsAllWords = false;
+                                if (state.contains(ifQuotes)) {
+                                    containsAllWords = true;
                                     break; // Exit the inner loop as soon as one statement doesn't contain all words
                                 }
                             }
                             if (containsAllWords) {
                                 doc1.append("idf-tf",df*doc1.getDouble("tf"));
                                 ret.add(doc1); // Add the document to the set if it contains all words
+                                
                             }
                         }
                     }
                 }
             }
 
-            // Convert the set of unique documents to an ArrayList if needed
-            
     }else{
-       
         for (String word : stems) {
             ArrayList<Document> result = DB.getDocumentsContainingWord(word,"Indexer");
+            if(result.isEmpty())
+            continue;
             Double df=1.0/(double)result.get(0).getInteger("documentCount");
             for (Document doc : result) {
                 List<Document> docs= (ArrayList<Document>) doc.get("documents");
@@ -68,38 +70,40 @@ class Index {
     }
     return  ret;
 }
-
 }
 
-public class queryProcessor {
-    
 
-    private Index index=new Index();
+public class queryProcessor {
+    private Index index;
     private String query;
     
     public queryProcessor(String query) {
         this.query=query;
+        index=new Index();
     }
 
     public ArrayList<Document> searchly() {
-
-        
         boolean isQoutes=isEnclosedInQuotes(query);
         String cleanWord = query.replaceAll("^\"|\"$", "");
-        if(isQoutes)
+        String ss=cleanWord;
+        if(isQoutes){
         System.out.println("isQoutes");
+        System.out.println(cleanWord);
+        }
         String[] words = preprocess(cleanWord);
         String[] stems = stemWords(words);
         ArrayList<Document> documents;
         if(isQoutes)
         {
             printWords(stems);
-            documents=(ArrayList<Document>) searchIndex(stems,isQoutes,cleanWord);
+            documents=(ArrayList<Document>) searchIndex(stems,isQoutes, ss);
         }else{
-                printWords(stems);
-               documents=(ArrayList<Document>) searchIndex(stems,isQoutes," ");
+            printWords(stems);
+            documents=(ArrayList<Document>) searchIndex(stems,isQoutes," ");
         }
-        return  mergeDocuments(documents);
+        ArrayList<Document>FinalQuery=mergeDocuments(documents);
+        addrankFD(FinalQuery);
+        return  FinalQuery;
     }
 
     private String[] preprocess(String query) {
@@ -153,7 +157,7 @@ public class queryProcessor {
         return index.search(stems,isQoutes,ifqoutes);
     }
 
-    public boolean isEnclosedInQuotes(String s) {
+    private boolean isEnclosedInQuotes(String s) {
         // Check if the modified string starts and ends with double quotes
         return s.startsWith("\"") && s.endsWith("\"");
     }
@@ -166,14 +170,23 @@ public class queryProcessor {
         }
     }
     
-    public void printDocuments(ArrayList<String> documents) {
+    private void printDocuments(ArrayList<String> documents) {
         System.out.println("Documents:");
         for (String doc : documents) {
             System.out.println("- " + doc);
         }
     }
-
-    
+    private void addrankFD(ArrayList<Document>documents){
+        for(Document doc:documents){
+            Double a=0.0;
+            if(doc.getBoolean("inHead"))
+            a=1.0;
+            if(doc.getBoolean("inHead"))
+            a+=0.5;
+            doc.put("rank", doc.getDouble("rank")+doc.getDouble("idf-tf")+a);
+         }
+         Collections.sort(documents, Comparator.comparingDouble(doc -> ((Document) doc).getDouble("rank")).reversed());
+    }
 }
 
 
