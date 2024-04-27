686d688d8be920373cb01c31b30d46b59a8ea254
Amr Mahmoud <amrmgoma6@gmail.com>
2024-04-27 04:44:27 +0300

    added pagination

--- backend/src/main/java/com/dragonball/backend/SearchController.java
+++ backend/src/main/java/com/dragonball/backend/SearchController.java
@@ -6,18 +6,38 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 
 import javax.management.Query;
+import java.util.ArrayList;
 import java.util.List;
+
 @CrossOrigin(origins = "http://127.0.0.1:5173")
 @RestController
 
 @RequestMapping("/api/search")
 public class SearchController {
 
+    private static final int PER_PAGE = 1;
     @Autowired
     private QueryProcessor queryProcessor;
+
     @GetMapping
-    public ResponseEntity<List<IndexerDocument>> search(@RequestParam String keyword) {
+    public ResponseEntity<PaginatedIndexerDocument> search(@RequestParam String keyword, @RequestParam(name = "page", defaultValue = "1") int page) {
         List<IndexerDocument> results = queryProcessor.searchly(keyword);
-        return new ResponseEntity<List<IndexerDocument>>(results, HttpStatus.OK);
+
+        PaginatedIndexerDocument paginatedIndexerDocument = new PaginatedIndexerDocument();
+        paginatedIndexerDocument.setPagesAvailable((int) Math.ceil((double) results.size() /(double) PER_PAGE));
+        paginatedIndexerDocument.setTotal(results.size());
+        paginatedIndexerDocument.setCurrentPage(page);
+        int startIndex = (page - 1) * PER_PAGE;
+        int endIndex = Math.min(startIndex + PER_PAGE, results.size());
+        List<IndexerDocument> resultsChunk;
+        if (startIndex >= results.size()) {
+            // If startIndex exceeds the list size, return an empty list
+            resultsChunk=  new ArrayList<>();
+        } else {
+            resultsChunk =  results.subList(startIndex, endIndex);
+        }
+        paginatedIndexerDocument.setResults(resultsChunk);
+        return new ResponseEntity<PaginatedIndexerDocument>(paginatedIndexerDocument, HttpStatus.OK);
     }
+
 }
